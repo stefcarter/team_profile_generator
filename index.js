a0e3d28d2a622f104c8744e8cd6e43c119ad931b
Steffan Carter <xisled09@gmail.com>
2022-05-17 22:59:37 -0400

    Small details added

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const prompt = {} => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-        }
-    ])
-}
